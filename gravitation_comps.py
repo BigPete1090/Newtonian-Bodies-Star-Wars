c3c0bc15ff250751f73c29e8f1b9918804111801
Peter Tenholder <peter1090ten@gmail.com>
2025-12-17 19:01:23 -0500

    Got it working it was just a misnamed variable thing also added energy checking

--- gravitation_comps.py
+++ gravitation_comps.py
@@ -13,6 +13,9 @@ from collections import defaultdict
 
 GRAVITATIONAL_CONSTANT = 6.67e-11
 all_objects = []
+initial_energy = 0
+showing_energy = 0
+current_energy = []
 
 objects = 0
 with open('objects.csv', mode='r') as file:
@@ -28,8 +31,8 @@ with open('objects.csv', mode='r') as file:
 
 
 
-frame_count = 1
-time_interval = (60*60*24) # NEED TO FIX
+frame_count = 0
+time_interval = 60*60*24
 
 # fix to accept one argument for r not 2
 def calculate_grav_force(m1, m2, r, component_1, component_2):
@@ -48,7 +51,7 @@ def calculate_acceleration(grav_force, mass):
     return grav_force / mass
 
 def calculate_new_position(r_k, v_k, a_k, t):
-    r_k1 = r_k + v_k * t + a_k * (t**2)
+    r_k1 = r_k + v_k * t + 0.5 * a_k * (t**2)
     return r_k1
 
 def calculate_new_velocity(v_k, a_k, a_k1, t):
@@ -63,6 +66,20 @@ def calculate_distance_between_objects(x1, y1, z1, x2, y2, z2):
     distance = np.sqrt(((x2-x1)**2)+((y2-y1)**2)+((z2-z1)**2))
     return distance
 
+def calculate_kinetic_energy(m, v):
+    return 0.5 * m * (v**2)
+
+def calculate_kinetic_energy(m, vx, vy, vz):
+    v = np.sqrt((vx**2) + (vy**2) + (vz**2))
+    return 0.5 * m * v
+
+def calculate_potential_energy(m1, m2, r):
+    u = -1*((GRAVITATIONAL_CONSTANT * m1 * m2)/r)
+    return u
+
+def calculate_mechanical_energy(u, k):
+    return k + u
+
 """def calculate_center_of_mass(mass_list, radius_list):
     #Assumes mass and radius are in ordered list
     if len(mass_list) != len(radius_list):
@@ -73,6 +90,10 @@ def calculate_distance_between_objects(x1, y1, z1, x2, y2, z2):
         continue
     return mass_list"""
 
+
+# Before running sim need to do the calculate center of mass of the whole area and shift everything
+
+
 def run_sim():
     global star_x, star_y, star_z
     global planet_1_x, planet_1_y, planet_1_z
@@ -80,22 +101,32 @@ def run_sim():
     global star_velocity_x, star_velocity_y, star_velocity_z
     global planet_1_velocity_x, planet_1_velocity_y, planet_1_velocity_z
     global all_objects
+    global initial_energy, current_energy, showing_energy
 
 
-
+    k = 0
+    u = 0
     distances_from_center = {}
     grav_forces = {}
     next_distances_from_center = {}
     all_current_accelerations = defaultdict(float)
     all_next_distances_each_direction = defaultdict(float)
+    new_positions = {}
+    new_velocities = {}
 
     all_next_accelerations = defaultdict(float)
     next_grav_forces = {}
-
-
+    # Calculate Kinetic Energy
+    for i in range(len(all_objects)):
+        object_mass = all_objects[i]['mass']
+        object_vx = all_objects[i]["velocity_x"]
+        object_vy = all_objects[i]["velocity_y"]
+        object_vz = all_objects[i]["velocity_z"]
+        k += calculate_kinetic_energy(object_mass, object_vx, object_vy, object_vz)
 
     for i in range(len(all_objects)):
         object_name = all_objects[i]['name']
+        
         object_x_m = au_to_meter(all_objects[i]['x'])
         object_y_m = au_to_meter(all_objects[i]['y'])
         object_z_m = au_to_meter(all_objects[i]['z'])
@@ -113,6 +144,8 @@ def run_sim():
         object_vx = all_objects[i]["velocity_x"]
         object_vy = all_objects[i]["velocity_y"]
         object_vz = all_objects[i]["velocity_z"]
+        print(f"\nStarting Object: {object_name}")
+
         
 
         object_x_m = au_to_meter(object_x)
@@ -122,7 +155,7 @@ def run_sim():
         object_distance = distances_from_center[object_name]
         
         for j in range(len(all_objects)):
-            if j != i:
+            if j > i:
                 second_object_name = all_objects[j]['name']
                 print(f"Calculating the gravity between object {object_name} and {second_object_name}")
                 second_object_mass = all_objects[j]['mass']
@@ -136,6 +169,8 @@ def run_sim():
 
                 object_distances = calculate_distance_between_objects(object_x_m, object_y_m, object_z_m, 
                                                                       second_object_x_m, second_object_y_m, second_object_z_m)
+                
+                u += calculate_potential_energy(object_mass, second_object_mass, object_distances)
 
 
                 # second_object_distance = distances_from_center[second_object_name]
@@ -171,19 +206,26 @@ def run_sim():
                     all_current_accelerations[object_name, 'y'] += calculate_acceleration(obj_grav_force_y, object_mass)
                     all_current_accelerations[object_name, 'z'] += calculate_acceleration(obj_grav_force_z, object_mass)
 
+                    all_current_accelerations[second_object_name, 'x'] += calculate_acceleration(second_obj_grav_force_x, second_object_mass)
+                    all_current_accelerations[second_object_name, 'y'] += calculate_acceleration(second_obj_grav_force_y, second_object_mass)
+                    all_current_accelerations[second_object_name, 'z'] += calculate_acceleration(second_obj_grav_force_z, second_object_mass)
 
         
         #Now that we have the total acceleration based on every object, calculate the position
         next_object_position_x = calculate_new_position(object_x_m, object_vx, all_current_accelerations[object_name, 'x'], time_interval)
         next_object_position_y = calculate_new_position(object_y_m, object_vy, all_current_accelerations[object_name, 'y'], time_interval)
         next_object_position_z = calculate_new_position(object_z_m, object_vz, all_current_accelerations[object_name, 'z'], time_interval)
-        next_object_distance = calculate_distance_from_center(next_object_position_x, next_object_position_y, next_object_position_z) #m
+
+        """print(f"DEBUG: {object_name} calculated next position (meters): x={next_object_position_x}, y={next_object_position_y}, z={next_object_position_z}")
+        print(f"DEBUG: {object_name} original position (meters): x={object_x_m}, y={object_y_m}, z={object_z_m}")
+        print(f"DEBUG: {object_name} velocity: vx={object_vx}, vy={object_vy}, vz={object_vz}")
+        print(f"DEBUG: {object_name} acceleration: ax={all_current_accelerations[object_name, 'x']}, ay={all_current_accelerations[object_name, 'y']}, az={all_current_accelerations[object_name, 'z']}")
+        """
 
         all_next_distances_each_direction[object_name, "x"] = next_object_position_x
         all_next_distances_each_direction[object_name, "y"] = next_object_position_y
         all_next_distances_each_direction[object_name, "z"] = next_object_position_z
 
-        next_distances_from_center[object_name] = next_object_distance
 
 
 
@@ -192,9 +234,11 @@ def run_sim():
         
         object_name = all_objects[i]['name']
         object_mass = all_objects[i]['mass']
+
         object_x = all_objects[i]['x']
         object_y = all_objects[i]['y']
         object_z = all_objects[i]['z']
+            
         object_vx = all_objects[i]["velocity_x"]
         object_vy = all_objects[i]["velocity_y"]
         object_vz = all_objects[i]["velocity_z"]
@@ -204,50 +248,36 @@ def run_sim():
         next_object_z = all_next_distances_each_direction[object_name, "z"]
 
 
-        print(f"Starting object {object_name}")
-
         object_ax = all_current_accelerations[(object_name, 'x')]
         object_ay = all_current_accelerations[(object_name, 'y')]
         object_az = all_current_accelerations[(object_name, 'z')]
 
-        object_x_m = au_to_meter(object_x)
-        object_y_m = au_to_meter(object_y)
-        object_z_m = au_to_meter(object_z)
-
-        next_object_distance = next_distances_from_center[object_name]
-
         for j in range(len(all_objects)):
-            if j != i:
+            if j > i:
 
                 second_object_name = all_objects[j]['name']
                 second_object_mass = all_objects[j]['mass']
-                second_object_x = all_objects[j]['x']
-                second_object_y = all_objects[j]['y']
-                second_object_z = all_objects[j]['z']
 
                 
                 next_second_object_x = all_next_distances_each_direction[second_object_name, "x"]
                 next_second_object_y = all_next_distances_each_direction[second_object_name, "y"]
                 next_second_object_z = all_next_distances_each_direction[second_object_name, "z"]
 
-                second_object_x_m = au_to_meter(second_object_x)
-                second_object_y_m = au_to_meter(second_object_y)
-                second_object_z_m = au_to_meter(second_object_z)
+
 
                 object_distances = calculate_distance_between_objects(next_object_x, next_object_y, next_object_z, 
                                                                       next_second_object_x, next_second_object_y, next_second_object_z)
                 
                 print(f"Calculating the next gravity between object {object_name} and {second_object_name}")
-                second_object_distance = distances_from_center[second_object_name]
                 if (next_grav_forces.get((object_name, second_object_name, "x"), None) is None or
                     next_grav_forces.get((object_name, second_object_name, "y")) is None or 
                     next_grav_forces.get((object_name, second_object_name, "z")) is None): 
 
                     obj_grav_force_x = calculate_grav_force(object_mass, second_object_mass, object_distances, 
                                                              next_object_x, next_second_object_x)
-                    obj_grav_force_y = calculate_grav_force(object_mass, second_object_mass, object_distance, 
+                    obj_grav_force_y = calculate_grav_force(object_mass, second_object_mass, object_distances, 
                                                              next_object_y, next_second_object_y)
-                    obj_grav_force_z = calculate_grav_force(object_mass, second_object_mass, object_distance, 
+                    obj_grav_force_z = calculate_grav_force(object_mass, second_object_mass, object_distances, 
                                                              next_object_z, next_second_object_z)
                     second_obj_grav_force_x = -obj_grav_force_x
                     second_obj_grav_force_y = -obj_grav_force_y
@@ -265,40 +295,77 @@ def run_sim():
                     all_next_accelerations[object_name, 'y'] += calculate_acceleration(obj_grav_force_y, object_mass)
                     all_next_accelerations[object_name, 'z'] += calculate_acceleration(obj_grav_force_z, object_mass)
 
+                    all_next_accelerations[second_object_name, 'x'] += calculate_acceleration(second_obj_grav_force_x, second_object_mass)
+                    all_next_accelerations[second_object_name, 'y'] += calculate_acceleration(second_obj_grav_force_y, second_object_mass)
+                    all_next_accelerations[second_object_name, 'z'] += calculate_acceleration(second_obj_grav_force_z, second_object_mass)
+
 
 
         next_object_acceleration_x = all_next_accelerations[object_name, 'x']
         next_object_acceleration_y = all_next_accelerations[object_name, 'y']
         next_object_acceleration_z = all_next_accelerations[object_name, 'z']
 
+        
+
         next_object_velocity_x = calculate_new_velocity(object_vx, object_ax, next_object_acceleration_x, time_interval)
         next_object_velocity_y = calculate_new_velocity(object_vy, object_ay, next_object_acceleration_y, time_interval)
         next_object_velocity_z = calculate_new_velocity(object_vz, object_az, next_object_acceleration_z, time_interval)
-        print(f"Found next {object_name} velocity from original position\n ({object_vx}, {object_vy}, {object_vz}) --> ({next_object_velocity_x}, {next_object_velocity_y}, {next_object_velocity_z}))")
+        print(f"Found next {object_name} velocity from original position\n ({object_vx:.3f}, {object_vy:.3f}, {object_vz:.3f}) --> ({next_object_velocity_x:.3f}, {next_object_velocity_y:.3f}, {next_object_velocity_z:.3f}))")
 
-        print(f"{object_name} Position all in AU: ({object_x}, {object_y}, {object_z}) --> ({meter_to_au(next_object_position_x)}, {meter_to_au(next_object_position_y)}, {meter_to_au(next_object_position_z)}))\n")
-    
+        print(f"{object_name} Position all in AU: ({object_x:.3f}, {object_y:.3f}, {object_z:.3f}) --> ({meter_to_au(next_object_x):.3f}, {meter_to_au(next_object_y):.3f}, {meter_to_au(next_object_z):.3f}))")
 
-        all_objects[i]['x'] = meter_to_au(next_object_position_x)
+        new_positions[object_name] = (next_object_x, next_object_y, next_object_z)
+        new_velocities[object_name] = (next_object_velocity_x, next_object_velocity_y, next_object_velocity_z)
+
+        """all_objects[i]['x'] = meter_to_au(next_object_position_x)
         all_objects[i]['y'] = meter_to_au(next_object_position_y)
         all_objects[i]['z'] = meter_to_au(next_object_position_z)
         all_objects[i]['velocity_x'] = next_object_velocity_x
         all_objects[i]['velocity_y'] = next_object_velocity_y
-        all_objects[i]['velocity_z'] = next_object_velocity_z
+        all_objects[i]['velocity_z'] = next_object_velocity_z"""    
 
-        print(f"{all_objects[i]['x']}, {all_objects[i]['y']}, {all_objects[i]['z']}, {all_objects[i]['velocity_x']}, {all_objects[i]['velocity_y']}, {all_objects[i]['velocity_z']}")
-    
+    for i in range(len(all_objects)):
+        object_name = all_objects[i]['name']
+
+        x_m, y_m, z_m = new_positions[object_name]
+        all_objects[i]['x'] = meter_to_au(x_m)
+        all_objects[i]['y'] = meter_to_au(y_m)
+        all_objects[i]['z'] = meter_to_au(z_m)
+
+        all_objects[i]['velocity_x'], all_objects[i]['velocity_y'], all_objects[i]['velocity_z'] = new_velocities[object_name]
+
+        # find mechanical energy
+        mechanical_energy = calculate_mechanical_energy(u, k)
+        if frame_count == 0:
+            initial_energy = mechanical_energy
+        if frame_count // 100 == 0:
+            showing_energy == mechanical_energy
+        current_energy.append(mechanical_energy)
+
+
     return
 
 
-fig = plt.figure()
+fig = plt.figure(figsize = (10, 5))
 
-ax = fig.add_subplot(projection="3d")
 
-ax.view_init(elev=90, azim=-90)
-ax.set_xlim(-10, 10)
-ax.set_ylim(-10, 10)
-ax.set_zlim(-10, 10)
+# The arguments 122 mean 1 row, 2 columns, 2nd plot
+# Crucially, specify the 'projection' argument
+ax1 = fig.add_subplot(1, 2, 1, projection='3d')
+
+
+
+
+
+
+ax1.view_init(elev=0, azim=-90)
+ax1.set_xlim(-3, 3)
+ax1.set_ylim(-3, 3)
+ax1.set_zlim(-3, 3)
+
+ax1.set_xlabel('X (AU)')
+ax1.set_ylabel('Y (AU)')
+ax1.set_zlabel('Z (AU)')
 
 traj_lines = []
 points = []
@@ -308,14 +375,24 @@ for obj in all_objects:
     obj["y_list"] = []
     obj["z_list"] = []
 
-    traj, = ax.plot([], [], [], '-', color=obj["color"], label=obj["name"])
-    point, = ax.plot([], [], [], 'o', color=obj["color"])
+    traj, = ax1.plot([], [], [], '-', color=obj["color"], label=obj["name"])
+    point, = ax1.plot([], [], [], 'o', color=obj["color"])
 
     traj_lines.append(traj)
     points.append(point)
 
 
-ax.legend()
+ax1.legend()
+
+ax2 = fig.add_subplot(1, 2, 2)
+x_data_energy = []
+y_data_energy = []
+line, = ax2.plot(x_data_energy, y_data_energy)
+
+ax2.set_title('Energy vs Frames')
+ax2.set_xlabel('Frame count aka days')
+ax2.set_ylabel('Mechanical Energy (J)')
+ax2.grid(True)
 
 
 def update(frame):
@@ -339,7 +416,21 @@ def update(frame):
         points[i].set_data([x], [y])
         points[i].set_3d_properties([z])
 
-    return (*traj_lines, *points)
+    new_x = frame
+    new_y = current_energy[-1]
+    x_data_energy.append(new_x)
+    y_data_energy.append(new_y)
+    line.set_data(x_data_energy, y_data_energy)
+    ax2.relim()
+    ax2.autoscale_view()
 
-anim = FuncAnimation(fig, update, frames=2, interval=500)
+    if new_x >= ax2.get_xlim()[1]:
+        ax2.set_xlim(new_x - 50, new_x + 50) # Shift the window
+        ax2.figure.canvas.draw() # Force redraw of axes
+    
+
+    return (*traj_lines, *points, line,)
+
+anim = FuncAnimation(fig, update, frames=30000, interval=0, repeat=False)
+plt.tight_layout()
 plt.show()
